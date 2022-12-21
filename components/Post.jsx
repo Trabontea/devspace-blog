80bce6a4b389983de26fa2dc5cd709d218924b23
Trabontea <danielmbotea@gmail.com>
2022-12-22 00:32:16 +0200

    Add posts

---
+++ components/Post.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Post = ({ post }) => {
+  return (
+    <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+      <Image
+        src={post.frontmatter.cover_image}
+        alt={post.frontmatter.title}
+        width={600}
+        height={420}
+        className="mb-4 rounded"
+      />
+      <div className="flex justify-between item-center">
+        <span className="font-light text-grey-600">
+          {post.frontmatter.date}
+        </span>
+        <div>{post.frontmatter.category}</div>
+      </div>
+
+      <div className="mt-2">
+        <Link
+          href={`/blog/${post.slug}`}
+          className="text-2xl text-gray-700 font-bold hover:underline"
+        >
+          {post.frontmatter.title}
+        </Link>
+        <p className="mt-2 text-gray-600">{post.frontmatter.excerpt}</p>
+      </div>
+
+      <div className="flex justify-between items-center mt-6'">
+        <Link
+          href={`/blog/${post.slug}`}
+          className="text-gray-900 hover:text-blue-600"
+        >
+          Read more
+        </Link>
+        <div className="flex items-center">
+          <img
+            src={post.frontmatter.author_image}
+            alt={post.frontmatter.author}
+            className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+          />
+          <h3 className="text-gray-700 font-bold">{post.frontmatter.author}</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,28 @@ import Layout from '../components/Layout'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import Link from 'next/link'
+import Post from '@/components/Post'
+import { sortByDate } from './../utils'
 
 export default function Home({ posts }) {
   console.log(posts)
   return (
     <Layout>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest posts!</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link
+        href="/blog"
+        className="block text-center border border-gray-500 text-gray-800 rounded-md 
+        py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 
+        focus:outline-none focus:shadow-outline w-full"
+      >
+        All Posts
+      </Link>
     </Layout>
   )
 }
@@ -24,18 +40,18 @@ export async function getStaticProps() {
     )
     //console.log(markdownWithMeta)
 
-    const { data: frontmattter } = matter(markdownWithMeta)
+    const { data: frontmatter } = matter(markdownWithMeta)
 
     // console.log(frontmattter)
     return {
       slug,
-      frontmattter,
+      frontmatter,
     }
   })
 
   console.log(posts)
 
   return {
-    props: { posts },
+    props: { posts: posts.sort(sortByDate).slice(0, 6) },
   }
 }
