1797ad4635b08c6b6ef87bec1760d6cc126a833c
Trabontea <danielmbotea@gmail.com>
2023-02-17 00:07:08 +0200

    add husky

--- pages/api/search.js
+++ pages/api/search.js
@@ -8,7 +8,7 @@ export default (req, res) => {
 
   if (process.env.NODE_ENV === 'production') {
     // Fetch from cache
-    //posts = require('../../cache/data').posts
+    posts = require('../../cache/data').posts
   } else {
     const files = fs.readdirSync(path.join('posts'))
 
